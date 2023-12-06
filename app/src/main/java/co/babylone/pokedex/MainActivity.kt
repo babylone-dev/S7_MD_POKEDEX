0860753a06e23d3cb9373068dd557a2ccc8e8024
Yann <yvillellas@gmail.com>
2023-12-07 00:09:39 +0100

    added sharedPreferences

--- app/src/main/java/co/babylone/pokedex/MainActivity.kt
+++ app/src/main/java/co/babylone/pokedex/MainActivity.kt
@@ -2,7 +2,6 @@ package co.babylone.pokedex
 
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -13,13 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_screen)
-        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+
+        val sharedPref = this.getSharedPreferences("co.babylone.pokedex", Context.MODE_PRIVATE)
 
         val loginButton = findViewById<Button>(R.id.login_button)
         loginButton.setOnClickListener {
             val username = findViewById<EditText>(R.id.username).text.toString()
             if (username.isNotEmpty()) {
-                Snackbar.make(findViewById(R.id.username), "Bienvenue $username", Snackbar.LENGTH_SHORT).show()
                 with (sharedPref.edit()) {
                     putString("username", username)
                     apply()
