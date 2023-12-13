7c52d6f5e31135eed98d3923312718e383223408
Yann <yvillellas@gmail.com>
2023-12-13 16:27:30 +0100

    images id pokemon

--- app/src/main/java/co/babylone/pokedex/PokemonAdapter.kt
+++ app/src/main/java/co/babylone/pokedex/PokemonAdapter.kt
@@ -1,12 +1,15 @@
 package co.babylone.pokedex
 
 import Pokemon
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+import com.squareup.picasso.Picasso
 
 
 class PokemonAdapter(private val pokemonList: List<Pokemon>) : RecyclerView.Adapter<PokemonAdapter.PokemonViewHolder>() {
@@ -18,8 +21,9 @@ class PokemonAdapter(private val pokemonList: List<Pokemon>) : RecyclerView.Adap
 
     override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
         val currentItem = pokemonList[position]
-        holder.textViewName.text = currentItem.name
-        // Utilisez Glide ou Picasso pour charger l'image
+        holder.textViewName.text = currentItem.name.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString() }
+        holder.textPokemonId.text = "#${currentItem.id.toString().padStart(3, '0')}"
+        Picasso.get().load(currentItem.image).into(holder.imagePokemon)
     }
 
     override fun getItemCount() = pokemonList.size
@@ -27,6 +31,7 @@ class PokemonAdapter(private val pokemonList: List<Pokemon>) : RecyclerView.Adap
     class PokemonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageView: ImageView = itemView.findViewById(R.id.imagePokemon)
         val textViewName: TextView = itemView.findViewById(R.id.textPokemonName)
-
+        val textPokemonId: TextView = itemView.findViewById(R.id.textPokemonId)
+        val imagePokemon: ImageView = itemView.findViewById(R.id.imagePokemon)
     }
 }
