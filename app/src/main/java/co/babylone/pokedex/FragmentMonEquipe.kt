de318193d4e5d1e3d81e9cbae4249e2e131bcda7
Elias Tourneux <elias_tourneux@connect-tech.sncf>
2023-12-14 22:31:43 +0100

    Add team's name

--- app/src/main/java/co/babylone/pokedex/FragmentMonEquipe.kt
+++ app/src/main/java/co/babylone/pokedex/FragmentMonEquipe.kt
@@ -7,7 +7,9 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import Pokeapi
+import android.content.Context
 import android.util.Log
+import android.widget.TextView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -20,6 +22,9 @@ class FragmentMonEquipe : Fragment(R.layout.fragment_mon_equipe) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val sharedPref = view.context.getSharedPreferences("co.babylone.pokedex", Context.MODE_PRIVATE)
+        val username = sharedPref.getString("username", "")
+        view.findViewById<TextView>(R.id.textViewEquipeTitle).text = username
 
         recyclerView = view.findViewById(R.id.recyclerViewMonEquipe)
         recyclerView.layoutManager = LinearLayoutManager(context)
