c728d7d8ac4203c11cb8c3300b8866ff72d2c903
Elias Tourneux <elias_tourneux@connect-tech.sncf>
2023-12-14 22:29:09 +0100

    Revert "Add team's name"
    This reverts commit c80e22219ab8993af425ff4970c8354847c79a88.

--- app/src/main/java/co/babylone/pokedex/MainActivity.kt
+++ app/src/main/java/co/babylone/pokedex/MainActivity.kt
@@ -9,7 +9,6 @@ import android.os.Bundle
 import android.view.inputmethod.InputBinding
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -33,7 +32,6 @@ class MainActivity : AppCompatActivity() {
                     putString("username", username)
                     apply()
                 }
-                findViewById<TextView>(R.id.textViewEquipeTitle).text = "$username's team"
                 startActivity(Intent(this, HomeActivity::class.java))
             } else {
                 Snackbar.make(findViewById(R.id.username), "Veuillez entrer un pseudonyme", Snackbar.LENGTH_SHORT).show()
